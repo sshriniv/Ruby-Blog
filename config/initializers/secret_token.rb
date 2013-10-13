21cbd7768cd0fc3e2e21b864b3744d1c2f547877
Sandeep Shrinivasan <Sandeep@Sandeep-Shrinivasans-MacBook-Pro.local>
2013-10-13 11:27:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubyblog::Application.config.secret_key_base = 'e36cae1cbb609692efb45dc4ab6c886f8c0d0081ee74b9bb9ebad1d81a2bd69c4f5e7ecbdbe9efb9c79058779d296bdc10b9f959fcc1fa20ebe1b368a74e6d5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rubyblog::Application.config.session_store :cookie_store, key: '_rubyblog_session'
